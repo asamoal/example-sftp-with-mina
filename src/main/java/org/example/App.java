0d9c0c84ea771b9c203d3a1ece9cd0d00455f9a9
Leslie @Data Mavens Ltd <2324551+asamoal@users.noreply.github.com>
2023-12-18 15:51:11 +0100

    Refactor SFTP Client setup process
    We've created granular methods for each setup process in SFTP client initialization. Specified SSH user credentials and SFTP keys' path are now class-level constants to make them reusable. We have also added specific exception handlers for GeneralSecurityException and IOException during the session authentication process. This increases readability and ease of future changes within the main function.
    Further, a Bouncy Castle cryptographic library has been added for better security and performance.
    
    This commit includes:
    - The extraction of SFTP client creation and session connection into separate methods
    - Addition of Bouncy Castle as a security provider for better performance and security
    - Improved error handling during session authentication

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,6 @@
 package org.example;
 
 // necessary imports
-
 import org.apache.sshd.client.SshClient;
 import org.apache.sshd.client.keyverifier.AcceptAllServerKeyVerifier;
 import org.apache.sshd.client.keyverifier.KnownHostsServerKeyVerifier;
@@ -14,46 +13,72 @@ import org.apache.sshd.sftp.client.SftpClientFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// optional imports to handle EdDSA provider error
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import java.security.Security;
+
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
 import java.security.KeyPair;
 import java.util.Collection;
 
 public class App {
+
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
     private static final Logger logger = LoggerFactory.getLogger(App.class);
+    private static final String user = "user";
+    private static final String host = "localhost";
+    private static final int port = 2222;
+    private static final Path privateKeyPath = Paths.get("/Users/leslieasamoa/CrushDrive/data_over_multi_sftp/config/keys/sftp-user-key");
 
     public static void main(String[] args) throws IOException {
-        try (SshClient client = SshClient.setUpDefaultClient()) {
-            client.start();
+        try (SshClient client = createSshClient()) {
+            connectToSession(client);
+        }
+    }
 
-            String user = "user";
-            String host = "localhost";
-            int port = 2222;
+    private static SshClient createSshClient() {
+        SshClient client = SshClient.setUpDefaultClient();
+        client.start();
 
-            Path privateKeyPath = Paths.get("/Users/leslieasamoa/CrushDrive/data_over_multi_sftp/config/keys/sftp-user-key");
-            FilePasswordProvider filePasswordProvider = FilePasswordProvider.EMPTY;
+//        FilePasswordProvider filePasswordProvider = FilePasswordProvider.EMPTY;
 
-            // If server's key not in known hosts, accept it (unsafe! only for this example)
-            Path knownHostsPath = Paths.get(System.getProperty("user.home"), ".ssh", "known_hosts");
-            KnownHostsServerKeyVerifier knownHostsVerifier = new KnownHostsServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE, knownHostsPath);
-            client.setServerKeyVerifier(knownHostsVerifier);
+        // If server's key not in known hosts, accept it (unsafe! only for this example)
+        Path knownHostsPath = Paths.get(System.getProperty("user.home"), ".ssh", "known_hosts");
+        KnownHostsServerKeyVerifier knownHostsVerifier = new KnownHostsServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE, knownHostsPath);
+        client.setServerKeyVerifier(knownHostsVerifier);
 
-            try (ClientSession session = client.connect(user, host, port).verify(10000).getSession()) {
-                Collection<KeyPair> keys = OpenSSHKeyPairResourceParser.INSTANCE.loadKeyPairs(session, new PathResource(privateKeyPath), filePasswordProvider);
-                session.addPublicKeyIdentity(keys.iterator().next());
-                session.auth().verify(10000);
+        return client;
+    }
 
-                SftpClient sftpClient = SftpClientFactory.instance().createSftpClient(session);
-                String remoteDirectory = "/";
+    private static void connectToSession(SshClient client) {
+        try (ClientSession session = client.connect(user, host, port).verify(10000).getSession()) {
+            authenticateSession(session);
 
-                for (SftpClient.DirEntry entry : sftpClient.readDir(remoteDirectory)) {
-                    logger.info(entry.getFilename());
-                }
+            SftpClient sftpClient = SftpClientFactory.instance().createSftpClient(session);
+            String remoteDirectory = "/";
 
-            } catch (Exception e) {
-                logger.error("An error occurred", e);
+            for (SftpClient.DirEntry entry : sftpClient.readDir(remoteDirectory)) {
+                logger.info(entry.getFilename());
             }
+
+        } catch (Exception e) {
+            logger.error("An error occurred", e);
+        }
+    }
+
+    private static void authenticateSession(ClientSession session) {
+        try {
+            Collection<KeyPair> keys = OpenSSHKeyPairResourceParser.INSTANCE.loadKeyPairs(session, new PathResource(privateKeyPath), FilePasswordProvider.EMPTY);
+            session.addPublicKeyIdentity(keys.iterator().next());
+            session.auth().verify(10000);
+        } catch (IOException | GeneralSecurityException e) {
+            logger.error("An error occurred during authentication", e);
         }
     }
 }
